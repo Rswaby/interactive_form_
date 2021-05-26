60296795ed203e279be985a32b5f686a5e7d10ce
Rohan Swaby <rohan_swaby51@yahoo.com>
2021-05-25 23:03:48 -0400

    added obvious errors

--- js/script.js
+++ js/script.js
@@ -18,6 +18,9 @@ const focusOnElementById = id =>{
     console.log('focusing on element with id =', id);
     document.getElementById(id).focus();
 };
+const getAllSubElements = (elementId, tag) => {
+    return document.getElementById(elementId).getElementsByTagName(tag);
+}
 const getElementById = id =>{
     return document.getElementById(id);
 }
@@ -32,50 +35,16 @@ const hideOrShowElementById = (id,action) =>{
         element.style.display = "none";
     }
 };
-// +++++++++++++++++++++++++++++++++++ validators +++++++++++++++++++++++++++++++++++++++++++++
-const nameValidator = () => {
-    console.log('++++++++++++ Name Validator ++++++++++++');
-    const name =  getElementById(NAME);
-    isValid = name.value.trim().length > 0 || false;
-    isValid ? hideOrShowElementById(NAMEHINT,HIDE) : hideOrShowElementById(NAMEHINT, SHOW);
-    console.log('isNameValid : ',isValid)
-    return isValid;
-}
-const emailValidator = () => {
-    const emailValue = String(getElementById('email').value);
-    const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
-    emailIsValid ?  hideOrShowElementById(EMAILHINT,HIDE) : hideOrShowElementById(EMAILHINT,SHOW);
-    return emailIsValid;
-}
 
-const activitiesValidator = () =>{
-    const isValid = STATE.activitiesCost > 0 || false;
-    isValid ? hideOrShowElementById(ACTIVITIESHINT, HIDE) : hideOrShowElementById(ACTIVITIESHINT, SHOW);
-    return isValid;
-}
-
-const creditCardValidator = () => {
-    const cardNumber = getElementById('cc-num').value;
-    const year = + getElementById('exp-year').value;
-    const cvv = getElementById('cvv').value;
-    const zip = getElementById('zip').value;
-
-    const visaCardRegex = /^4[0-9]{12}(?:[0-9]{3})?$/;
-    const amex = /^3[47][0-9]{13}$/; 
-    const masterCardRegx =  /^5[1-5][0-9]{14}$/; 
-    const zipRegex = /^\d{5}$/;
-    const cvvRegex = /^\d{3}$/;
-
-    const isCardNumberValid = visaCardRegex.test(cardNumber) || masterCardRegx.test(cardNumber) || amex.test(cardNumber);
-    const isYearValid = isNaN(year);
-    const isValidZipCode = zipRegex.test(zip);
-    const isCvv = cvvRegex.test(cvv);
-
-    isCardNumberValid ? hideOrShowElementById(CREDITCARDHINTS.card,HIDE) : hideOrShowElementById(CREDITCARDHINTS.card,SHOW);
-    isValidZipCode ? hideOrShowElementById(CREDITCARDHINTS.zip,HIDE) : hideOrShowElementById(CREDITCARDHINTS.zip,SHOW);
-    isCvv ? hideOrShowElementById(CREDITCARDHINTS.cvv,HIDE) : hideOrShowElementById(CREDITCARDHINTS.cvv,SHOW);
-
-    return isCardNumberValid && isValidZipCode && isCvv;
+const hideOrShowObviousErrors = (id,action) => {
+    const parentElement = getElementById(id).parentElement;
+    if (action === SHOW){
+        console.log("Showing obv error: ", id)
+        parentElement.classList.add('not-valid');
+    } else if(action === HIDE) {
+        parentElement.classList.remove('not-valid');
+        console.log("Hiding obv error: ", id)
+    }
 }
 
 const showPaymentMethodfor = () => {
@@ -94,6 +63,89 @@ const showPaymentMethodfor = () => {
     }
 }
 
+// +++++++++++++++++++++++++++++++++++ validators +++++++++++++++++++++++++++++++++++++++++++++
+const nameValidator = () => {
+    console.log('++++++++++++ Name Validator ++++++++++++');
+    const name =  getElementById(NAME);
+    isValid = name.value.trim().length > 0 || false;
+    if(isValid){
+        hideOrShowElementById(NAMEHINT,HIDE);
+        hideOrShowObviousErrors(NAME,HIDE);
+    }else{
+        hideOrShowElementById(NAMEHINT, SHOW);
+        hideOrShowObviousErrors(NAME,SHOW);
+    }
+    console.log('isNameValid : ',isValid)
+    return isValid;
+}
+const emailValidator = () => {
+    const emailValue = String(getElementById('email').value);
+    const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+
+    if(emailIsValid){
+        hideOrShowElementById(EMAILHINT,HIDE);
+        hideOrShowObviousErrors('email',HIDE);
+    }else{
+        hideOrShowElementById(EMAILHINT,SHOW);
+        hideOrShowObviousErrors('email',SHOW);
+    }
+    return emailIsValid;
+}
+
+const activitiesValidator = () =>{
+    const isValid = STATE.activitiesCost > 0 || false;
+    if(isValid){
+        hideOrShowElementById(ACTIVITIESHINT, HIDE)
+        hideOrShowObviousErrors(ACTIVITIESHINT,HIDE) //since parent of acitvities hint is feildset
+    } else {
+        hideOrShowElementById(ACTIVITIESHINT, SHOW)
+        hideOrShowObviousErrors(ACTIVITIESHINT,SHOW) //since parent of acitvities hint is feildset
+    }
+    return isValid;
+}
+
+const creditCardValidator = () => {
+    const cardNumber = getElementById('cc-num').value;
+    const year = + getElementById('exp-year').value;
+    const cvv = getElementById('cvv').value;
+    const zip = getElementById('zip').value;
+
+    const visaCardRegex = /^4[0-9]{12}(?:[0-9]{3})?$/;
+    const amex = /^3[47][0-9]{13}$/; 
+    const masterCardRegx =  /^5[1-5][0-9]{14}$/; 
+    const zipRegex = /^\d{5}$/;
+    const cvvRegex = /^\d{3}$/;
+
+    const isCardNumberValid = visaCardRegex.test(cardNumber) || masterCardRegx.test(cardNumber) || amex.test(cardNumber);
+    const isYearValid = isNaN(year); //not needed? 
+    const isValidZipCode = zipRegex.test(zip);
+    const isValidCvv = cvvRegex.test(cvv);
+    // cc-num
+    if(isCardNumberValid){
+        hideOrShowElementById(CREDITCARDHINTS.card,HIDE);
+        hideOrShowObviousErrors(CREDITCARDHINTS.card, HIDE);
+    }else{
+        hideOrShowElementById(CREDITCARDHINTS.card,SHOW);
+        hideOrShowObviousErrors(CREDITCARDHINTS.card, SHOW);
+    }
+    // cc-zip
+    if(isValidZipCode){
+        hideOrShowElementById(CREDITCARDHINTS.zip,HIDE);
+        hideOrShowObviousErrors(CREDITCARDHINTS.zip,HIDE);
+    }else{
+        hideOrShowElementById(CREDITCARDHINTS.zip,SHOW);
+        hideOrShowObviousErrors(CREDITCARDHINTS.zip,SHOW);
+    }
+    // cvv
+    if(isValidCvv){
+        hideOrShowElementById(CREDITCARDHINTS.cvv,HIDE);
+        hideOrShowObviousErrors(CREDITCARDHINTS.cvv,HIDE)
+    }else{
+        hideOrShowElementById(CREDITCARDHINTS.cvv,SHOW);
+        hideOrShowObviousErrors(CREDITCARDHINTS.cvv,SHOW)
+    }
+    return isCardNumberValid && isValidZipCode && isCvv;
+}
 // defaults 
 focusOnElementById(NAME);
 hideOrShowElementById(OTHERJOBROLE,HIDE);
@@ -105,9 +157,11 @@ const jobSection = getElementById('title');
 const shirtDesigns = getElementById('shirt-designs');
 const designOptions = getElementById('color');
 const activities = getElementById('activities');
+const activitesInputElemets = getAllSubElements('activities','input');
 const displayTotal = getElementById('activities-cost');
 const payment = getElementById('payment');
 const form = document.querySelector("form");
+
 
 jobSection.addEventListener('change',(e) => {
     console.log('++++++++++++ Job Section ++++++++++++')
@@ -143,6 +197,22 @@ activities.addEventListener('change',(e) => {
     isChecked ? STATE.activitiesCost += cost : STATE.activitiesCost -= cost;
     displayTotal.innerHTML = `Total: $${STATE.activitiesCost}`;    
 });
+
+for (let input of activitesInputElemets){
+    const parentLabel = input.parentElement;
+    ['focus','blur'].forEach(action => {
+        input.addEventListener(action,(e) => {
+            console.log('++++++++++++ activites Input Focus ++++++++++++');
+            if(action === 'focus'){
+                console.log('input element focusing for: ',input.name);
+                parentLabel.classList.add('focus');
+            }else{
+                console.log('input element blur: ',input.name);
+                parentLabel.classList.remove('focus');
+            }
+        })
+    });
+}
 
 payment.addEventListener('change',(e) => {
     console.log('++++++++++++ payment ++++++++++++');
